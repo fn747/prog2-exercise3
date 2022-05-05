b166787b03812cbdb2db6a720a5794ccd7122e51
fn747 <92045446+fn747@users.noreply.github.com>
2022-05-05 23:39:12 +0200

    Done.

--- src/main/java/weather/ctrl/WeatherController.java
+++ src/main/java/weather/ctrl/WeatherController.java
@@ -6,38 +6,87 @@ import tk.plogitech.darksky.forecast.APIKey;
 import tk.plogitech.darksky.forecast.ForecastRequest;
 import tk.plogitech.darksky.forecast.ForecastRequestBuilder;
 import tk.plogitech.darksky.forecast.GeoCoordinates;
-import tk.plogitech.darksky.forecast.model.Forecast;
+import tk.plogitech.darksky.forecast.model.*;
+
+import java.util.List;
 
 public class WeatherController {
-    
-    private String apiKey = "Enter your API key";
-    
+
+    private String apiKey = "ab5c55091bfde0864c41b337f1c66af5";;
+    private DarkSkyJacksonClient client = new DarkSkyJacksonClient();
 
     public void process(GeoCoordinates location) {
         System.out.println("process "+location); //$NON-NLS-1$
-		//Forecast data = getData();
-		
-		//TODO implement Error handling 
-		
-		//TODO implement methods for
-		// highest temperature 
-		// average temperature 
-		// count the daily values
-		
-		// implement a Comparator for the Windspeed 
-		
-	}
-    
-    
-    public Forecast getData(GeoCoordinates location) {
-		ForecastRequest request = new ForecastRequestBuilder()
-                .key(new APIKey(apiKey))
+        //Forecast data = getData();
+
+        //
+
+        //TODO implement Error handling
+
+        //TODO implement methods for
+        // highest temperature
+        // average temperature
+        // count the daily values
+
+        Forecast forecast = my_try_catch(client,apiRequest(apiKey,location));
+        double highTemp = getHighestTemp(location);
+        double averageTemp = getAverageTemp(location);
+
+
+        // implement a Comparator for the Windspeed
+
+    }
+    public Forecast my_try_catch(DarkSkyJacksonClient client, ForecastRequest forecastRequest) {
+        Forecast forecast = new Forecast();
+        try {
+            forecast = client.forecast(forecastRequest);
+
+        }
+        catch (Exception e) {
+            System.out.println("try_catch_test error");
+        }
+        return forecast;
+    }
+    public ForecastRequest apiRequest(String apiKey, GeoCoordinates location) {
+        ForecastRequest request = new ForecastRequestBuilder()
+                .key(new APIKey("ab5c55091bfde0864c41b337f1c66af5"))
                 .location(location)
                 .build();
 
+        return request;
+    }
+    public Double getHighestTemp(GeoCoordinates location) {
+
         DarkSkyJacksonClient client = new DarkSkyJacksonClient();
-        //Forecast forecast = client.forecast(request);
-        
-        return null;
+        Forecast forecast = my_try_catch(client,apiRequest(apiKey,location));
+        List<DailyDataPoint> dailyDataPoints = forecast.getDaily().getData();
+
+        return dailyDataPoints.stream().mapToDouble(DailyDataPoint::getTemperatureHigh).max().orElseThrow();
+    }
+
+    public Double getAverageTemp(GeoCoordinates location) {
+
+        DarkSkyJacksonClient client = new DarkSkyJacksonClient();
+        Forecast forecast = my_try_catch(client,apiRequest(apiKey,location));
+        List<DailyDataPoint> dailyDataPoints = forecast.getDaily().getData();
+
+        return dailyDataPoints.stream().mapToDouble(DailyDataPoint::getTemperatureHigh).average().orElseThrow();
+    }
+
+    public GeoCoordinates getLocation(Longitude x, Latitude y) {
+        return new GeoCoordinates(x,y);
+    }
+
+
+
+    public static void main(String[] args) {
+        WeatherController wc = new WeatherController();
+
+        Longitude x = new Longitude(40.0);
+        Latitude y = new Latitude(18.0);
+
+        GeoCoordinates location = wc.getLocation(x,y);
+        System.out.println(wc.getHighestTemp(location));
+        System.out.println(wc.getAverageTemp(location));
     }
 }
--- src/main/java/weather/ui/UserInterface.java
+++ src/main/java/weather/ui/UserInterface.java
@@ -3,38 +3,68 @@ package weather.ui;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Scanner;
 
+import tk.plogitech.darksky.forecast.GeoCoordinates;
+import tk.plogitech.darksky.forecast.model.Latitude;
+import tk.plogitech.darksky.forecast.model.Longitude;
 import weather.ctrl.WeatherController;
 
-public class UserInterface 
+public class UserInterface
 {
 
 	private WeatherController ctrl = new WeatherController();
+	private Longitude x;
+	private Latitude y;
+	//GeoCoordinates location;
 
 	public void getWeatherForCity1(){
-		
-		//TODO enter the coordinates 
-		ctrl.process(null);
+		this.x = new Longitude(25.0);
+		this.y = new Latitude(18.0);
+		//DONE enter the coordinates
+		ctrl.process(ctrl.getLocation(this.x,this.y));
 
 	}
 
 	public void getWeatherForCity2(){
-		//TODO enter the coordinates 
-		ctrl.process(null);
+		//DONE enter the coordinates
+		this.x = new Longitude(35.0);
+		this.y = new Latitude(20.0);
+		ctrl.process(ctrl.getLocation(this.x,this.y));
 
 	}
 
 	public void getWeatherForCity3(){
-		//TODO enter the coordinates 
-		ctrl.process(null);
+		//DONE enter the coordinates
+		this.x = new Longitude(45.0);
+		this.y = new Latitude(40.0);
+		ctrl.process(ctrl.getLocation(this.x,this.y));
 
-}
-	
+	}
+
 	public void getWeatherByCoordinates() {
-		//TODO read the coordinates from the cmd
-		//TODO enter the coordinates 
-		ctrl.process(null);
-		
+		//DONE read the coordinates from the cmd
+		//DONE enter the coordinates
+		System.out.println("Please enter longitude:");
+		Scanner enter_longitude = new Scanner(System.in);
+		double enter_long = enter_longitude.nextDouble();
+		Longitude longitude_console = new Longitude(enter_long);
+
+		System.out.println("Please enter latitude:");
+		Scanner enter_latitude = new Scanner(System.in);
+		double enter_lat = enter_latitude.nextDouble();
+		Latitude latitude_console = new Latitude(enter_lat);
+
+		GeoCoordinates console = new GeoCoordinates(longitude_console, latitude_console);
+
+		ctrl.process(console);
+
+	}
+
+	@Override
+	public String toString() {
+		return "highest temp: " + ctrl.getHighestTemp(ctrl.getLocation(this.x, this.y)) +
+				"average temp: " + ctrl.getAverageTemp(ctrl.getLocation(this.x, this.y));
 	}
 
 	public void start() {
@@ -47,13 +77,13 @@ public class UserInterface
 		menu.insert("q", "Quit", null);
 		Runnable choice;
 		while ((choice = menu.exec()) != null) {
-			 choice.run();
+			choice.run();
 		}
 		System.out.println("Program finished");
 	}
 
 
-	protected String readLine() 
+	protected String readLine()
 	{
 		String value = "\0";
 		BufferedReader inReader = new BufferedReader(new InputStreamReader(System.in));
@@ -64,7 +94,7 @@ public class UserInterface
 		return value.trim();
 	}
 
-	protected Double readDouble(int lowerlimit, int upperlimit) 
+	protected Double readDouble(int lowerlimit, int upperlimit)
 	{
 		Double number = null;
 		while(number == null) {
